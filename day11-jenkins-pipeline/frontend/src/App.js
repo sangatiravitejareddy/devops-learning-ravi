3b97b7d7ebd5ebe36520142c39e3d40cfb425e0f
Sangatiravitejareddy <mail.ravitejareddy@gmail.com>
2025-06-26 17:06:54 +0000

    Day 11 Jenkins Pipeline CI/CD

---
+++ day11-jenkins-pipeline/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    fetch('/api/data')
+      .then(res => res.json())
+      .then(data => setTime(data.time));
+  }, []);
+
+  return <h1>Server Time: {time}</h1>;
+}
+
+export default App;
+
+
